abd783fd470aa83d85ed2b20d54a605e5010f15a
tkieuvt <thanhkieu13052004@gmail.com>
2023-08-30 17:09:27 +0700

    chuong1

--- bai4.py
+++ bai4.py
@@ -2,7 +2,7 @@ n = int(input("n = "))
 print("SNT tu 1 den", n, "là:")
 for num in range(2, n + 1):
     SNT = True
-    for i in range(2, num):
+    for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             SNT = False
             break
